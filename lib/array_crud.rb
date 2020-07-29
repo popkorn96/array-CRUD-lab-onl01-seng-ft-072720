b62bf0b2537ae6b2467eea56a65e3f353ace032c
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 12:47:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["one","two","three"]
-  puts << "four"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
