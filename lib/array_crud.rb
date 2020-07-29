18364b5eb559f63985f576d28be95e90738dfe3d
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 12:44:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   famous_cats = ["one","two","three"]
-  famous_cats.push ("four")
+  famous_cats <<"four"
 end
 
 def add_element_to_start_of_array(array, element)
